3ecb26daca9f5e629cfdeeb641d33ab25163664e
MaxSaleh <maxsaleh2003@gmail.com>
2022-08-14 17:47:32 -0600

    Updated mods.toml & packages

---
+++ src/main/java/me/maxish0t/dynamicvehicles/DynamicVehicles.java
@@ -0,0 +1,13 @@
+package me.maxish0t.dynamicvehicles;
+
+import me.maxish0t.dynamicvehicles.utilities.ModReference;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ModReference.MOD_ID)
+public class DynamicVehicles {
+
+    public DynamicVehicles() {
+        
+    }
+
+}
---
+++ src/main/java/me/maxish0t/dynamicvehicles/utilities/ModReference.java
@@ -0,0 +1,15 @@
+package me.maxish0t.dynamicvehicles.utilities;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+public class ModReference {
+
+    // Mod Information
+    public static final String MOD_ID = "dynamicvehicles";
+    public static final String MOD_NAME = "Dynamic Vehicles";
+
+    // Mod Extra
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+}
